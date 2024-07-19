43f0066d6c296a39e40fb8ae3962c1b6e22f3f0d
Joaco2008 <107818656+Joaco2008@users.noreply.github.com>
2024-07-19 00:31:10 -0300

    Add files via upload

---
+++ version 1.1/index.js
@@ -0,0 +1,9 @@
+function modal() {
+    document.getElementById("Windows__Modal").style.display = "block";
+    document.getElementById("body").style.overflow = "hidden";
+}
+
+function ocultar() {
+    document.getElementById("Windows__Modal").style.display = "none";
+    document.getElementById("body").style.overflow = "scroll"
+}
